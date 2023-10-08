1957766740029edc3585b340cb758dd62441d359
Ravi Pandey <67542571+Pandey9800@users.noreply.github.com>
2023-10-08 20:53:41 +0530

    changes

--- script.js
+++ script.js
@@ -61,6 +61,32 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+/* Smooth Scrolling */
+
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll(".scroll-fade-in");
+
+    const options = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.2,
+    };
+
+    const observer = new IntersectionObserver(function (entries, observer) {
+        entries.forEach(function (entry) {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            } else {
+                entry.target.classList.remove("visible"); // Remove the class when not in viewport
+            }
+        });
+    }, options);
+
+    sections.forEach(function (section) {
+        observer.observe(section);
+    });
+});
+
 
 
 
