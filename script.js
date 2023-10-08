a47b9b30cf983b1c06056dc0a6486bebb2126fbb
Ravi Pandey <67542571+Pandey9800@users.noreply.github.com>
2023-10-08 20:00:24 +0530

    Site for Meta

---
+++ script.js
@@ -0,0 +1,72 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const slider = document.querySelector(".slider");
+    const frame = slider.querySelector(".slider-frame");
+    const images = slider.querySelectorAll(".slider-image");
+
+    let currentIndex = 0;
+
+    function showImage(index) {
+        frame.style.transform = `translateX(-${index * 100}%)`;
+    }
+
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % images.length;
+        showImage(currentIndex);
+    }
+
+    // Automatically load the next image every 3 seconds (adjust the time as needed)
+    setInterval(nextSlide, 3000);
+
+    // Initially show the first image
+    showImage(currentIndex);
+});
+
+// Product Slider 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const container = document.getElementById("product-container");
+    const products = container.querySelectorAll(".product");
+
+    let currentIndex = 0;
+    let visibleProducts = 4;
+
+    function showProducts() {
+        for (let i = 0; i < products.length; i++) {
+            if (i >= currentIndex && i < currentIndex + visibleProducts) {
+                products[i].style.display = "block";
+            } else {
+                products[i].style.display = "none";
+            }
+        }
+
+        currentIndex = (currentIndex + 1) % (products.length - visibleProducts + 1);
+    }
+
+    // Automatically show products every 3 seconds (adjust the time as needed)
+    setInterval(showProducts, 3000);
+
+    // Initially show the first set of products
+    showProducts();
+});
+
+/* Menu For Mobile */
+document.addEventListener("DOMContentLoaded", function () {
+    const mobileNavButton = document.querySelector(".mobile-nav-button");
+    const navLinks = document.querySelector(".nav-links");
+
+    // Add an event listener to the mobile navigation button
+    mobileNavButton.addEventListener("click", function () {
+        // Toggle a CSS class to show/hide the menu with animation
+        navLinks.classList.toggle("show-mobile-menu");
+    });
+});
+
+
+
+
+
+
+
+
+
+
